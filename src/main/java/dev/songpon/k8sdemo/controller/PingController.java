308476fd4ac7ec3be9a156f70ffa4e40ea9e6bf6
Songpon Imyen <tum@songpon-system.local>
2022-08-11 15:47:40 +0700

    first commit

---
+++ src/main/java/dev/songpon/k8sdemo/controller/PingController.java
@@ -0,0 +1,13 @@
+package dev.songpon.k8sdemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingController {
+
+    @GetMapping("/ping")
+    public String ping() {
+        return "pong";
+    }
+}
